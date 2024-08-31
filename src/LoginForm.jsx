7ae08543fe4fa49387b8832bd28e770bb5e718d4
Sammaann <sammanthapachhetri@gmail.com>
2024-08-31 15:19:26 +0545

    first commit

---
+++ src/LoginForm.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./LoginForm.css";
+import { FcGoogle } from "react-icons/fc";
+import { MdFacebook } from "react-icons/md";
+
+
+const LoginForm = () => {
+  return (
+    <div className="login-container">
+      <div className="form-section">
+        <h2>Log In</h2>
+        <p>Welcome back! Please enter your details</p>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter your email"
+              required
+    
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="forgot-password">
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" className="login-button">
+            Log In
+          </button>
+          <div className="or-divider">Or Continue With</div>
+          <div className="social-login">
+            <button type="button" className="google-login">
+            <FcGoogle />
+            Google
+            </button>
+            
+            <button type="button" className="facebook-login">
+            <MdFacebook />
+              Facebook
+            </button>
+          </div>
+        </form>
+        <p className="signup-text">
+          Don’t have an account? <a href="#">Sign up</a>
+        </p>
+      </div>
+      <div className="image-section">
+        <img src="https://img.freepik.com/free-photo/young-woman-exercising-with-her-headphones_23-2149127345.jpg" alt="Login" />
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
